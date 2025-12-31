fa9c28ba53d02659b17100c3357fe65fadcbd97c
ZdapX <stokmlmurmer150@gmail.com>
2026-01-01 01:31:17 +0700

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,31 +1,10 @@
 
-const { Redis } = require('@upstash/redis');
+const mongoose = require('mongoose');
 
-const redis = new Redis({
-  url: 'https://growing-firefly-50232.upstash.io',
-  token: 'AcQ4AAIncDFlYjI2ZWM2ODhmOGQ0N2YwOTI1Njg5ZDA3ZjRjMDdhMHAxNTAyMzI',
+const MessageSchema = new mongoose.Schema({
+    to: { type: String, required: true, lowercase: true },
+    text: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
 });
 
-const Message = {
-  send: async (to, text) => {
-    const cleanTo = to.trim().toLowerCase();
-    const msgData = { 
-      text: text, 
-      time: new Date().toISOString() 
-    };
-    await redis.lpush(`msgs:${cleanTo}`, msgData);
-  },
-  get: async (username) => {
-    const cleanUser = username.trim().toLowerCase();
-    const data = await redis.lrange(`msgs:${cleanUser}`, 0, -1);
-    return data || [];
-  },
-  // FITUR BARU: Hapus Pesan
-  delete: async (username, msgObjectString) => {
-    const cleanUser = username.trim().toLowerCase();
-    // Menghapus pesan yang isinya persis sama
-    await redis.lrem(`msgs:${cleanUser}`, 0, msgObjectString);
-  }
-};
-
-module.exports = Message;
+module.exports = mongoose.models.Message || mongoose.model('Message', MessageSchema);
