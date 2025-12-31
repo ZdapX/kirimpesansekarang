94fee82e8b1a74e1c4e62699fd271da5143c3da5
ZdapX <stokmlmurmer150@gmail.com>
2026-01-01 01:30:30 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,37 +1,9 @@
 
-const { Redis } = require('@upstash/redis');
+const mongoose = require('mongoose');
 
-const redis = new Redis({
-  url: 'https://growing-firefly-50232.upstash.io',
-  token: 'AcQ4AAIncDFlYjI2ZWM2ODhmOGQ0N2YwOTI1Njg5ZDA3ZjRjMDdhMHAxNTAyMzI',
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true, lowercase: true, trim: true },
+    password: { type: String, required: true }
 });
 
-const User = {
-  create: async (username, password) => {
-    if(!username || !password) return false;
-    
-    // Hilangkan spasi di depan/belakang
-    const cleanUser = username.trim().toLowerCase();
-    const cleanPass = password.trim();
-
-    const exists = await redis.get(`user:${cleanUser}`);
-    if (exists) return false;
-
-    await redis.set(`user:${cleanUser}`, cleanPass);
-    return true;
-  },
-  
-  login: async (username, password) => {
-    if(!username || !password) return false;
-
-    const cleanUser = username.trim().toLowerCase();
-    const cleanPass = password.trim();
-
-    const savedPass = await redis.get(`user:${cleanUser}`);
-    
-    // Konversi keduanya ke String untuk memastikan perbandingan valid
-    return String(savedPass) === String(cleanPass);
-  }
-};
-
-module.exports = User;
+module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
